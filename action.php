1a119b48922e82090b84ccf70e0324ca05702af5
Soushi <soushi@modxcms-jp.com>
2012-07-01 13:24:10 +0900

    2012/06/26 脆弱性修正

--- action.php
+++ action.php
@@ -4,13 +4,13 @@ define('MODX_API_MODE', true);
 require_once("{$base_path}index.php");
 $modx->db->connect();
 $modx->getSettings();
-$modx->invokeEvent('OnWebPageInit');
 if(isset($_GET['include']))
 {
 	$path = $_GET['include'];
 	if(strpos($path, 'manager/')===0 && substr($path,strrpos($path,'.'))==='.php')
 	{
-		$path = MODX_BASE_PATH . $path;
+		$path = str_replace('\\','/',realpath(MODX_BASE_PATH . $path));
+		if(strpos($path,MODX_MANAGER_PATH)!==0) exit;
 		if(file_exists($path)) include_once($path);
 	}
 }
